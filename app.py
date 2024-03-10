0b5df601efbfb27c7a694b5fc155cbe2e2dc7ca6
Trainersudhanshu <sudhanshutest123@gmail.com>
2024-03-10 14:14:09 +0530

    updated the code logic

--- app.py
+++ app.py
@@ -5,12 +5,23 @@ from urllib.parse import quote
 
 def create_app():
     app = Flask(__name__)
+    x =4
+    y =    777
 
     @app.route('/')
     def home():
         return 'Welcome Guys12345232323!'
 
     return app
+
+def a():
+    print("test..")
+    a()
+def test():
+    p=8
+
+def test():
+    q=10
 
 if __name__ == '__main__':
     app = create_app()
