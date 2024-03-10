1921359aa6a9388c2fb9a207d647390b7ad3dede
Trainersudhanshu <sudhanshutest123@gmail.com>
2024-03-10 12:17:31 +0530

    updated test app

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Welcome Guys!'
+    expected_text = 'Welcome Guys123!'
     assert expected_text.encode() in response.data
